baf1b281593caaa7757d24ed1f8ba4f27491513d
David Sehi <david.sehi2016@gmail.com>
2023-01-17 13:36:52 +0000

    edit LoginForm component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 import { App } from './App'
 import './assets/index.css'
 
@@ -9,6 +10,7 @@ const root = createRoot(document.getElementById('root'))
 root.render(
   <StrictMode>
     <Router>
+      <Toaster />
       <App />
     </Router>
   </StrictMode>
